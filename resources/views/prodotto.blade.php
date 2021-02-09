ec11811efd662a94023ccfb505a9d0e594f7dd5e
adri <adri.kolaj@gmail.com>
2021-02-09 18:45:48 +0100

    pagina dettaglio prodotto

--- resources/views/prodotto.blade.php
+++ resources/views/prodotto.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 
-<h1>ciao </h1>
+  <div class="wrapper">
+    <div class="container">
+      <h1>{{ $prodotto["titolo"] }}</h1>
+      <img src="{{ $prodotto["src-h"] }}" alt="{{ $prodotto["titolo"] }}">
+      <img src="{{ $prodotto["src-p"] }}" alt="{{ $prodotto["titolo"] }}">
+      <p>{!! $prodotto["descrizione"] !!}</p>
+    </div>
+  </div>
 
 @endsection
